ac0aae277dedc90e60d447b16116a5e5f9584d01
lysol <lysol@moil.monoperative.net>
2011-06-27 21:02:34 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>
+Derek Arnold
+</title>
+<meta name="description" content="Derek Arnold, a person from Iowa." />
+<meta name="keywords" content="derek arnold,derek,arnold,douglbutt,lysol,iowa,mason city,north iowa,pymblr,congress" />
+<meta name="google-site-verification" content="r_BkAkM4qxgWUrrvcS2981DR6W14Eeja59efnHNBRJ8" />
+<meta name=”ROBOTS” content=”NOODP”>
+<script type="text/javascript">
+lloogg_clientid = "21210200db197786";
+</script>
+<script type="text/javascript" src="http://lloogg.com/l.js?c=21210200db197786">
+</script>
+
+<style type="text/css">
+@font-face {
+		font-family: 'Grandesign Neue Serif';
+		src: url('fonts/Grandesign Neue Serif.ttf');
+}
+
+@font-face {
+		font-family: 'Grandesign Neue Serif';
+		font-weight: bold;
+		src: url('fonts/Grandesign Neue Serif Bold.ttf');
+}
+
+@font-face {
+		font-family: 'Grandesign Neue Serif';
+		font-style: italic;
+		src: url('fonts/Grandesign Neue Serif Italic.ttf');
+}
+
+@font-face {
+		font-family: 'Grandesign Neue Serif';
+		font-weight: bold;
+		font-style: italic;
+		src: url('fonts/Grandesign Neue Serif Bold Italic.ttf');
+}
+
+body {
+  padding: 0;
+  margin 0;
+  font-size: 18px;
+  font-family: 'Grandesign Neue Serif','Garamond','Times','Times New Roman', serif;
+  color: #000025;
+  -webkit-font-smoothing: antialiased;
+
+}
+
+a,a:hover,a:active,a:visited {
+  color: #587aa9;
+}
+
+
+#list_cont {
+    padding: 5em;	
+	margin-top: 1em;
+	padding-top: 1em;
+	border-top: 1px solid #ECECEC;
+}
+
+.bullet li {
+  background-repeat: no-repeat;
+  background-position: 0px 1px;
+}
+
+#main {
+    margin: 0;
+	padding: 5em;
+	padding-top: 2em;
+	padding-bottom: 0;
+}
+
+</style>
+</head>
+
+<body>
+<div id="main">
+<h2>I'm Derek Arnold, and these are some things.</h2>
+</div>
+<div id="list_cont">
+<ul class="bullet">
+<li>My <a href="http://tumblr.dderek.com">Tumblr</a> site.</li>
+<li>My <a href="http://github.com/lysol">Github</a> page.<br />
+  <ul>
+    <li>My <a href="http://github.com/lysol/douglbutt">IRC bot</a>.</li>
+	<li>My <a href="http://github.com/lysol/pymblr">Python Tumblr library</a> fork.</li>
+    <li>My <a href="http://github.com/lysol/congress">Distributed Hash Table</a> project.</li>
+ 	<li>My <a href="http://github.com/lysol/plpytablelog">PostgreSQL Table Log</a>.</li> 
+  </ul>
+</li>
+<li>My <a href="http://twitter.com/DerekArnold">Twitter</a> account.</li>
+<li>My <a href="http://dderek.com/blight">photo blog</a>, where I post pictures of local buildings.</li>
+<li>My <a href="http://flickr.com/photos/w">Flickr</a> account, where I post other photos.</li>
+<li>My <a href="derek@dderek.com">email</a> address.</li>
+</ul>
+
+<p>
+That's all the time I have for now, but tune in next time for Derek Arnold's website.
+</p>
+
+</div>
+<script src="http://tweetboard.com/tb.js?v=1.0&amp;user=derekarnold" type="text/javascript"></script>
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-658194-15']);
+   _gaq.push(['_trackPageview']);
+
+  (function() {
+     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+</script>
+
+</body>
+</html>
